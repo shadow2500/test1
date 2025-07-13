0de77455ce7997f3a0203b2c6c7efaa1aed4217d
shadow2500 <sajadkeneshlo1@gmail.com>
2025-07-13 22:37:32 +0330

    part#10

--- js/script.js
+++ js/script.js
@@ -1,4 +1,37 @@
   document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
+
+
+
+
+
+
   });
+
+  // promise example
+
+  // console.log(1);
+
+  // new Promise((resolve, reject) => {
+  //   setTimeout(function(){
+  //     console.log(2);
+  //     resolve("ok");
+  //     //reject("no ok")
+  //   })
+  // }).then(
+  //   function(response){
+  //     console.log(response);
+  //     console.log(3);
+      
+      
+  //   })
+
+
+
+  // )//.catch(
+  // function(error){
+  //   console.log(error);
+    
+  // }
+  
--- js/user.js
+++ js/user.js
@@ -12,5 +12,33 @@ document.addEventListener('DOMContentLoaded', function() {
       document.querySelector('.add_user_modal').classList.remove('show')
       e.target.classList.add('dis-none')
     })
-  });
+    
 
+    setTimeout(function(){
+      fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((json) =>{
+            let html = ''
+      for(const user of json){
+        html = html + `<tr>
+                          <td>${user.id}</td>
+                          <td>${user.name}</td>
+                          <td>${user.username}</td>
+                          <td>${user.email}</td>
+                          <td>${user.address.city}</td>
+                          <td>     
+                          <i class="material-icons red-text m-l-5 m-r-5">delete_forever</i>
+                          <i class="material-icons orange-text darken-3 m-l-5 m-r-5">edit</i>
+                          </td>
+                    
+                        </tr>`
+      }
+      document.querySelector('.desktop_table_body').innerHTML = html
+      })
+    },5000)
+
+  
+
+
+  
+})
